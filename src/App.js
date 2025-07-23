2f9ca405e120817251cf211aa2dc28cb828c55e8
Cleyton Melo <cleytonw11@gmail.com>
2025-07-23 19:50:57 -0300

    Criação do projeto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './paginas/Home';
+import Login from './paginas/Login';
+
+function App() {
+ 
+  return (
+  // <Login/>
+  <Home/>
+)
+}
+
+export default App;
